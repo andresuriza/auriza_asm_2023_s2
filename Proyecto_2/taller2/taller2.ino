f9cd8a5913622bb76bb7b7c647a3468e421f0431
Abner2111 <abneraq73@gmail.com>
2023-10-18 15:32:54 -0600

    added plotting of modulation

--- Proyecto_2/taller2/taller2.ino
+++ Proyecto_2/taller2/taller2.ino
@@ -1,26 +1,96 @@
 #include <arduinoFFT.h>
 
 const int OUTPUT_FREQUENCY = 100; // Frecuencia de la señal senoidal en Hz
-const uint16_t SAMPLES = 128; //This value MUST ALWAYS be a power of 2
-const double SAMPLING_FREQUENCY = 300; //Hz, must be less than 10000 due to ADC ///126
-
-double signal[SAMPLES];
+const uint16_t SAMPLES = 256; //This value MUST ALWAYS be a power of 2
+const double SAMPLING_FREQUENCY = 400; //Hz, must be less than 10000 due to ADC ///126
+const double CARRIER_FREQUENCY = 1000;
+double signall[SAMPLES];
+double carrier[SAMPLES];
+double modulatedSignal[SAMPLES];
+double demodulatedSignal[SAMPLES];
 double vReal[SAMPLES];
 double vImag[SAMPLES];
 
+bool signallGenerated;
 unsigned int samplingPeriod; 
+unsigned int carrierPeriod;
 
 arduinoFFT FFT = arduinoFFT();
+void generateSineWave(double frequency) {
+  double now = micros();
+  int i = 0;
+  while (i < SAMPLES) {
+    if((micros()-now) >= samplingPeriod){
+      double time = now;
+      signall[i] = sin(2 * PI * frequency * time/1000000);
+      now = micros();
+      i++;
+    }
+  }
+}
+
+void generateCarrierCos(double frequency){
+  double now = micros();
+  int i = 0;
+  while(i<SAMPLES){
+    if((micros()-now) >= samplingPeriod){
+      double time = now/1000000;
+      carrier[i] = sin(2*PI*frequency*time);
+      now = micros();
+      i++;
+    }
+  }
+}
+
+void amplitude_modulate_signal(){
+  for (int i = 0;i<SAMPLES;i++){
+    modulatedSignal[i] = signall[i]*carrier[i];
+  }
+}
+
+void amplitude_demodulate_signal(){
+  for (int i = 0;i<SAMPLES;i++){
+    demodulatedSignal[i] = modulatedSignal[i]*carrier[i];
+  }
+}
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   samplingPeriod = round((1.0/SAMPLING_FREQUENCY)*1000000);
+  signallGenerated = false;
 }
 
 void loop() {
-  generateSineWave(OUTPUT_FREQUENCY);
+  if(!signallGenerated){
+    Serial.println("Generating signals...");
+    generateSineWave(OUTPUT_FREQUENCY);
+    generateCarrierCos(CARRIER_FREQUENCY);
+    amplitude_modulate_signal();
+    amplitude_demodulate_signal();
+    
+    for (int i = 0;i<SAMPLES;i++){
+      Serial.print("Senal_senoidal:");
+      Serial.print(signall[i]);
+      Serial.print(",");
+
+      Serial.print("Senal_carrier:");
+      Serial.print(carrier[i]);
+      Serial.print(",");
+
+      Serial.print("Senal_modulada:");
+      Serial.print(modulatedSignal[i]);
+      Serial.print(",");
+
+      Serial.print("Senal_demodulada:");
+      Serial.println(demodulatedSignal[i]);
+    }
+    signallGenerated = true;
+  }
+  
+
+  
   for (int i = 0; i < SAMPLES; i++) {
-    vReal[i] = signal[i];
+    vReal[i] = signall[i];
     vImag[i] = 0;
   }
   FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_RECTANGLE, FFT_FORWARD);
@@ -28,26 +98,13 @@ void loop() {
   FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
   FFT.DCRemoval(vReal, SAMPLES);
   double dominantFrecuency = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
-
+  /*
   Serial.print("Frecuencia (Hz): ");
   Serial.println(dominantFrecuency); //Print out what frequency is the most dominant.
-
-  delay(5000);
+  */
+  delay(1000);
 }
 
-void generateSineWave(double frequency) {
-  double now = micros();
-  int i = 0;
-  while (i < SAMPLES) {
-    if((micros()-now) >= samplingPeriod){
-      double time = now;
-      signal[i] = sin(2 * PI * frequency * time/1000000);
-      Serial.println("Senal senoidal: ");
-      Serial.println(signal[i]);
-      now = micros();
-      i++;
-    }
-  }
-}
+
 
 
