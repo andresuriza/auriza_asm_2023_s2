84ac263645921e4d1f9aa407ed14834119634e80
Abner2111 <abneraq73@gmail.com>
2023-10-19 15:06:52 -0600

    increased sample frequency and added freq printing for all signals

--- Proyecto_2/taller2/taller2.ino
+++ Proyecto_2/taller2/taller2.ino
@@ -1,9 +1,9 @@
 #include <arduinoFFT.h>
 
-const int OUTPUT_FREQUENCY = 100; // Frecuencia de la señal senoidal en Hz
-const uint16_t SAMPLES = 256; //This value MUST ALWAYS be a power of 2
-const double SAMPLING_FREQUENCY = 400; //Hz, must be less than 10000 due to ADC ///126
-const double CARRIER_FREQUENCY = 1000;
+const int OUTPUT_FREQUENCY =  50;// Frecuencia de la señal senoidal en Hz
+const uint16_t SAMPLES = 1024; //This value MUST ALWAYS be a power of 2
+const double SAMPLING_FREQUENCY = 1500; //Hz, must be less than 10000 due to ADC ///126
+const double CARRIER_FREQUENCY = 300;
 double signall[SAMPLES];
 double carrier[SAMPLES];
 double modulatedSignal[SAMPLES];
@@ -14,6 +14,8 @@ double vImag[SAMPLES];
 bool signallGenerated;
 unsigned int samplingPeriod; 
 unsigned int carrierPeriod;
+
+double dominantFrecuency;
 
 arduinoFFT FFT = arduinoFFT();
 void generateSineWave(double frequency) {
@@ -34,8 +36,8 @@ void generateCarrierCos(double frequency){
   int i = 0;
   while(i<SAMPLES){
     if((micros()-now) >= samplingPeriod){
-      double time = now/1000000;
-      carrier[i] = sin(2*PI*frequency*time);
+      double time = now;
+      carrier[i] = cos(2* PI * frequency * time/1000000);
       now = micros();
       i++;
     }
@@ -93,16 +95,62 @@ void loop() {
     vReal[i] = signall[i];
     vImag[i] = 0;
   }
+
   FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_RECTANGLE, FFT_FORWARD);
   FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
   FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
   FFT.DCRemoval(vReal, SAMPLES);
-  double dominantFrecuency = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
-  /*
-  Serial.print("Frecuencia (Hz): ");
+  dominantFrecuency = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+  
+  Serial.print("Frecuencia senal (Hz): ");
   Serial.println(dominantFrecuency); //Print out what frequency is the most dominant.
-  */
-  delay(1000);
+  
+  for (int i = 0; i < SAMPLES; i++) {
+    vReal[i] = carrier[i];
+    vImag[i] = 0;
+  }
+
+  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_RECTANGLE, FFT_FORWARD);
+  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+  FFT.DCRemoval(vReal, SAMPLES);
+  dominantFrecuency = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+  
+  Serial.print("Frecuencia carrier (Hz): ");
+  Serial.println(dominantFrecuency); //Print out what frequency is the most dominant.
+  delay(500);
+
+  for (int i = 0; i < SAMPLES; i++) {
+    vReal[i] = modulatedSignal[i];
+    vImag[i] = 0;
+  }
+
+  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_RECTANGLE, FFT_FORWARD);
+  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+  FFT.DCRemoval(vReal, SAMPLES);
+  dominantFrecuency = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+  
+  Serial.print("Frecuencia modulada (Hz): ");
+  Serial.println(dominantFrecuency); //Print out what frequency is the most dominant.
+  delay(500);
+
+  for (int i = 0; i < SAMPLES; i++) {
+    vReal[i] = demodulatedSignal[i];
+    vImag[i] = 0;
+  }
+
+  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_RECTANGLE, FFT_FORWARD);
+  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+  FFT.DCRemoval(vReal, SAMPLES);
+  dominantFrecuency = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+  
+  Serial.print("Frecuencia demodulada (Hz): ");
+  Serial.println(dominantFrecuency); //Print out what frequency is the most dominant.
+  Serial.println();
+  Serial.println();
+  delay(5000);
 }
 
 
