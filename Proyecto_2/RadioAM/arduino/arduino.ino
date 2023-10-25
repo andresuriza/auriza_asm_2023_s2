32a644eaf193ff71938adfc9d516a646e3bb0ea5
ItsRichard7 <ricardobm@estudiantec.cr>
2023-10-25 15:40:27 -0600

    Optimizaciones

--- Proyecto_2/RadioAM/arduino/arduino.ino
+++ Proyecto_2/RadioAM/arduino/arduino.ino
@@ -3,12 +3,17 @@
 #define PIN_SENSOR A0
 #define PIN_SALIDA 8  
 
-const int OUTPUT_FREQUENCY =  40; // Frecuencia de la señal senoidal en Hz
-const double SAMPLING_FREQUENCY = 3000; //Hz, must be less than 10000 due to ADC ///126
-const double CARRIER_FREQUENCY_1 = 1000; // Frecuencia de la primera portadora
-const double CARRIER_FREQUENCY_2 = 100; // Frecuencia de 
+const int OUTPUT_FREQUENCY =  100; // Frecuencia del tono senoidal enviado
+const double SAMPLING_FREQUENCY = 8000; // Frecuencia a la que se envian las muestras
+const double CARRIER_FREQUENCY_1 = 3000; // Frecuencia de la primera portadora
+const double CARRIER_FREQUENCY_2 = 500; // Frecuencia de la segunda portadora
+
+const double primerFrecuencia = 2 * PI * CARRIER_FREQUENCY_1;
+const double segundaFrecuencia = 2 * PI * CARRIER_FREQUENCY_2;
+const double tonoSenoidal = 2 * PI * OUTPUT_FREQUENCY;
 
 unsigned int samplingPeriod; 
+
 
 void setup() {
   Serial.begin(9600);
@@ -23,14 +28,14 @@ void loop() {
   while(true){
     if((micros()-now) >= samplingPeriod){
       double primerMensaje = analogRead(PIN_SENSOR);
-      double primerPortadora = cos(2 * PI * CARRIER_FREQUENCY_1 * time);
+      double primerPortadora = cos(primerFrecuencia * time);
       double primerSenalModulada = primerMensaje * primerPortadora;
 
-      double segundoMensaje = 200 * cos(2 * PI * OUTPUT_FREQUENCY * time);
-      double segundaPortadora =  cos(2 * PI * CARRIER_FREQUENCY_2 * time);
+      double segundoMensaje = 200 * cos(tonoSenoidal * time);
+      double segundaPortadora =  cos(segundaFrecuencia * time);
       double segundaSenalModulada = segundoMensaje * segundaPortadora;
 
-      double mensajeAux = primerSenalModulada + segundaSenalModulada; 
+      int mensajeCombinado = primerSenalModulada + segundaSenalModulada; 
 
       //int mensajeFinal = map(mensajeAux, -1023, 1023, 0, 255);
 
@@ -50,8 +55,9 @@ void loop() {
       Serial.print(",");
       Serial.println(segundaSenalModulada);*/
 
-      //Serial.println(mensajeAux);
-      Serial.Write(mensajeAux);
+      //Serial.println(mensajeCombinado);
+
+      Serial.println(mensajeCombinado);
       break;
     }
   }
