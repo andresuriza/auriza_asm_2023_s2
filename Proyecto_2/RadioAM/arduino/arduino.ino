9de59db516773b2c87df585546f6177a2100d884
ItsRichard7 <ricardobm@estudiantec.cr>
2023-10-25 18:15:16 -0600

    Eliminar salida analógica

--- Proyecto_2/RadioAM/arduino/arduino.ino
+++ Proyecto_2/RadioAM/arduino/arduino.ino
@@ -1,16 +1,15 @@
 #include <arduinoFFT.h>
 
 #define PIN_SENSOR A0
-#define PIN_SALIDA 8  
 
 const int OUTPUT_FREQUENCY =  100; // Frecuencia del tono senoidal enviado
 const double SAMPLING_FREQUENCY = 8000; // Frecuencia a la que se envian las muestras
 const double CARRIER_FREQUENCY_1 = 3000; // Frecuencia de la primera portadora
 const double CARRIER_FREQUENCY_2 = 500; // Frecuencia de la segunda portadora
 
-const double primerFrecuencia = 2 * PI * CARRIER_FREQUENCY_1;
-const double segundaFrecuencia = 2 * PI * CARRIER_FREQUENCY_2;
-const double tonoSenoidal = 2 * PI * OUTPUT_FREQUENCY;
+const double primerFrecuencia = 2 * PI * CARRIER_FREQUENCY_1; // Frecuencia en radianes
+const double segundaFrecuencia = 2 * PI * CARRIER_FREQUENCY_2; // Frecuencia en radianes
+const double tonoSenoidal = 2 * PI * OUTPUT_FREQUENCY; // Frecuencia en radianes
 
 unsigned int samplingPeriod; 
 
@@ -18,7 +17,6 @@ unsigned int samplingPeriod;
 void setup() {
   Serial.begin(9600);
   pinMode(PIN_SENSOR, INPUT);
-  pinMode(PIN_SALIDA, OUTPUT);
   samplingPeriod = round((1.0/SAMPLING_FREQUENCY)*1000000);
 }
 
@@ -36,10 +34,6 @@ void loop() {
       double segundaSenalModulada = segundoMensaje * segundaPortadora;
 
       int mensajeCombinado = primerSenalModulada + segundaSenalModulada; 
-
-      //int mensajeFinal = map(mensajeAux, -1023, 1023, 0, 255);
-
-      //analogWrite(PIN_SALIDA, mensajeFinal);
 
       /*
       Serial.print(primerMensaje);
